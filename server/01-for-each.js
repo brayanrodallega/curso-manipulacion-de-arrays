1a66836f9a44b5b03afb166189191e513741f947
brayanrodallega <thebrayan1a1@outlook.com>
2023-03-16 20:08:39 -0500

    forEach

---
+++ server/01-for-each.js
@@ -0,0 +1,9 @@
+const letters = ['a', 'b', 'c', 'd', 'e'];
+
+// for loop
+for (let i = 0; i < letters.length; i++) {
+    console.log('for: ', letters[i]);
+}
+
+// forEach
+letters.forEach(letter => console.log('forEach ', letter));
